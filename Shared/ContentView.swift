a22841d04a46b30b5784f65308dda1e05bd0810a
balaji guna <balajiguna@mail.ru>
2022-07-07 12:11:51 +0530

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,103 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by BALAJI GUNA on 03/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var messageText = ""
+    @State var messages: [String] = ["Welcome to WolfBot!"]
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("WolfBot")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Image(systemName: "bubble.left.fill")
+                    .font(.system(size: 26))
+                    .foregroundColor(Color.orange)
+            }
+            
+            ScrollView {
+                ForEach(messages, id: \.self) { message in
+                    // If the message contains [USER], that means it's us
+                    if message.contains("[USER]") {
+                        let newMessage = message.replacingOccurrences(of: "[USER]", with: "")
+                        
+                        // User message styles
+                        HStack {
+                            Spacer()
+                            Text(newMessage)
+                                .padding()
+                                .foregroundColor(Color.white)
+                                .background(Color.orange.opacity(0.8))
+                                .cornerRadius(10)
+                                .padding(.horizontal, 16)
+                                .padding(.bottom, 10)
+                        }
+                    } else {
+                        
+                        // Bot message styles
+                        HStack {
+                            Text(message)
+                                .padding()
+                                .background(Color.gray.opacity(0.15))
+                                .cornerRadius(10)
+                                .padding(.horizontal, 16)
+                                .padding(.bottom, 10)
+                            Spacer()
+                        }
+                    }
+                    
+                }.rotationEffect(.degrees(180))
+            }
+            .rotationEffect(.degrees(180))
+            .background(Color.gray.opacity(0.1))
+            
+            
+            // Contains the Message bar
+            HStack {
+                TextField("Type something", text: $messageText)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .onSubmit {
+                        sendMessage(message: messageText)
+                    }
+                
+                Button {
+                    sendMessage(message: messageText)
+                } label: {
+                    Image(systemName: "paperplane.fill")
+                }
+                .font(.system(size: 26))
+                .padding(.horizontal, 10)
+            }
+            .padding()
+        }
+    }
+    
+    func sendMessage(message: String) {
+        withAnimation {
+            messages.append("[USER]" + message)
+            self.messageText = ""
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                withAnimation {
+                    messages.append(getBotResponse(message: message))
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/WolfBotApp.swift
@@ -0,0 +1,20 @@
+//
+//  WolfBotApp.swift
+//  Shared
+//
+//  Created by BALAJI GUNA on 03/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WolfBotApp: App {
+   
+ 
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
